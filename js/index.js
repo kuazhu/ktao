1c3f4d42e4236f71b25040b6654d4976278600df
TomChen <76876201@qq.com>
2018-07-05 09:27:36 +0800

    loadHtml once function

--- js/index.js
+++ js/index.js
@@ -2,43 +2,44 @@
 * @Author: TomChen
 * @Date:   2018-06-08 20:17:35
 * @Last Modified by:   TomChen
-* @Last Modified time: 2018-06-15 20:25:53
+* @Last Modified time: 2018-07-05 09:24:47
 */
 ;(function($){
+
+	function loadHtmlOnce($elem,callBack){
+		//获取需要请求的地址
+		var loadUrl = $elem.data('load');
+		//如果页面上没有设置请求地址直接返回
+		if(!loadUrl) return;
+
+		var isLoaded = $elem.data('isLoaded');
+		//如果已经加载过数据了直接返回
+		if(isLoaded) return;		
+		//如果有请求地址,发送请求获取数据
+		$.getJSON(loadUrl,function(data){
+			console.log('get data ...',data);
+			callBack($elem,data);
+		});		
+	}
 
 	/*顶部下拉菜单开始*/
 	var $menu = $('.nav-site .dropdown');
 	
 	$menu.on('dropdown-show',function(ev){
-		// console.log(this);
-		var $this = $(this);
-		//当需要显示时从服务器获取数据并且加载
-
-		//获取需要请求的地址
-		var loadUrl = $this.data('load');
-		//如果页面上没有设置请求地址直接返回
-		if(!loadUrl) return;
-
-		var isLoaded = $this.data('isLoaded');
-		//如果已经加载过数据了直接返回
-		if(isLoaded) return;
-		
-		//如果有请求地址,发送请求获取数据
-		$.getJSON(loadUrl,function(data){
-			console.log(data);
-			var html = '';
-			for(var i = 0;i<data.length;i++){
-				html += '<li><a href="'+data[i].url+'" class="menu-item">'+data[i].name+'</a></li>';
-			}
-			//模拟网络延时
-			setTimeout(function(){
-				$this.find('.dropdown-layer').html(html);
-				$this.data('isLoaded',true);
-			},1000);
-		});
-
+		loadHtmlOnce($(this),buildMenuItem)
 	});
-	
+	//构建菜单并加重
+	function buildMenuItem($elem,data){
+		var html = '';
+		for(var i = 0;i<data.length;i++){
+			html += '<li><a href="'+data[i].url+'" class="menu-item">'+data[i].name+'</a></li>';
+		}
+		//模拟网络延时
+		setTimeout(function(){
+			$elem.find('.dropdown-layer').html(html);
+			$elem.data('isLoaded',true);
+		},1000);
+	}
 	$menu.dropdown({
 		css3:false,
 		js:true,
@@ -94,69 +95,26 @@
 	var $category = $('.category .dropdown');
 
 	$category.on('dropdown-show',function(ev){
-		// console.log(this);
-		var $this = $(this);
-		//当需要显示时从服务器获取数据并且加载
 
-		//获取需要请求的地址
-		var loadUrl = $this.data('load');
-		//如果页面上没有设置请求地址直接返回
-		if(!loadUrl) return;
-
-		var isLoaded = $this.data('isLoaded');
-		//如果已经加载过数据了直接返回
-		if(isLoaded) return;
-		
-		//如果有请求地址,发送请求获取数据
-		$.getJSON(loadUrl,function(data){
-
-			/*
-				[
-					{
-						"title": "电视",//dt
-						"items": [//dd
-							"抢亿元红包",
-							"合资品牌",
-							"国产品牌",
-							"互联网品牌"
-						]
-					},
-					{
-						"title": "洗衣机",//dt
-						"items": [//dd
-							"抢亿元红包",
-							"合资品牌",
-							"国产品牌",
-							"互联网品牌"
-						]
-					},					
-				]
-						<dl class="category-details clearfix">
-							<dt class="category-details-title fl">
-								<a href="#" class="category-details-title-link">电视</a>
-							</dt>
-							<dd class="category-details-item fl">
-								<a href="#" class="link">曲面电视</a>
-								<a href="#" class="link">超薄电视</a>					
-							</dd>
-						</dl>				
-			*/
-			var html = '';
-			for(var i = 0;i<data.length;i++){
-				html += '<dl class="category-details clearfix"><dt class="category-details-title fl"><a href="#" class="category-details-title-link">'+data[i].title+'</a></dt><dd class="category-details-item fl">';
-				for(var j = 0;j<data[i].items.length;j++){
-					html += '<a href="#" class="link">'+data[i].items[j]+'</a>'
-				}
-				html += '</dd></dl>';
-			}
-			//模拟网络延时
-			setTimeout(function(){
-				$this.find('.dropdown-layer').html(html);
-				$this.data('isLoaded',true);
-			},1000);
-		});
+		loadHtmlOnce($(this),buildCategorItem);
 
 	});
+	function buildCategorItem($elem,data){
+		var html = '';
+		for(var i = 0;i<data.length;i++){
+			html += '<dl class="category-details clearfix"><dt class="category-details-title fl"><a href="#" class="category-details-title-link">'+data[i].title+'</a></dt><dd class="category-details-item fl">';
+			for(var j = 0;j<data[i].items.length;j++){
+				html += '<a href="#" class="link">'+data[i].items[j]+'</a>'
+			}
+			html += '</dd></dl>';
+		}
+		//模拟网络延时
+		setTimeout(function(){
+			$elem.find('.dropdown-layer').html(html);
+			$elem.data('isLoaded',true);
+		},1000);		
+	}
+
 	$category.dropdown({
 		css3:false,
 		js:true,
